5c83dc1e215be499693bfcd552620c85805193a1
Nathan <130991624+NathanCart@users.noreply.github.com>
2024-08-16 11:22:21 +0100

    Add total pages

--- api/custom_pagination.py
+++ api/custom_pagination.py
@@ -1,7 +1,19 @@
 from rest_framework import pagination
+from rest_framework.response import Response
 
 class CustomPagination(pagination.PageNumberPagination):
     page_size = 2
     page_size_query_param = 'per_page'
     max_page_size = 50
     page_query_param = 'page_num'
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+               'next': self.get_next_link(),
+               'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'results': data
+        })
