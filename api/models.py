be671006b458302451b66ec68abcb5cecb2585f9
Nathan <130991624+NathanCart@users.noreply.github.com>
2024-08-21 20:56:12 +0100

    Max length

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.contrib.postgres.fields import ArrayField
 
 class User(models.Model):
     spotify_id = models.CharField(max_length=100, unique=True)
-    artists = ArrayField(models.CharField(blank=True),null=True,
+    artists = ArrayField(models.CharField(blank=True, max_length=255),null=True,
         blank=True, 
         default=list, 
         max_length=255)
